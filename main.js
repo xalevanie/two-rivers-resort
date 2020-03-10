d9ea54d56ca0f1fc8b47c3f1264486f852fac7c6
xalevanie <maddybrine91@gmail.com>
2020-03-10 16:02:05 -0700

    First
    Uploadedededed

---
+++ main.js
@@ -0,0 +1,35 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+$(document).ready(function () {
+    "use strict";
+    $("#clickMe").click(function (e) {
+        alert("hello world!");
+    });
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+/* Set the width of the sidebar to 250px (show it) */
+
+function openNav() {document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {document.getElementById("mySidepanel").style.width = "0";
+}
