f783f59496773d785f79b64b05f56d5fdd887a05
xalevanie <maddybrine91@gmail.com>
2020-03-23 17:20:29 -0700

    Adding content
    Added facbook portal,
    minimized and added nav images,
    stylistic tweaks all over.

--- main.js
+++ main.js
@@ -35,3 +35,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
 }
+
+
+
+/* Facebook insert */
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
